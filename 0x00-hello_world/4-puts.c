af6ed2e42f1c674821581336f2aa17c03a8d169c
Norhn <nourhanessam102@gmail.com>
2023-03-16 13:10:00 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
  *
  * Description: A C program that prints with put function
  *
- * Resturn: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
          puts("\"Programming is like building a multilingual puzzle");
