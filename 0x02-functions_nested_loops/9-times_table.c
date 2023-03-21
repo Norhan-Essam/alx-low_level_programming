74cf8ce21bed944d0a3827a95a1cf235a2262bf1
Norhn <nourhanessam102@gmail.com>
2023-03-21 12:59:31 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-		for (muit = 1; muit <= 9; mult++)
+		for (muit = 1; muit <= 9; muit++)
 		{
 			_putchar(',');
 			_putchar(' ');
