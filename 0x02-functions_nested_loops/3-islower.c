7ef31e7f1e03aa0c4998c3ae753bb048d2d31214
Norhn <nourhanessam102@gmail.com>
2023-03-21 04:30:37 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Checks for lowercase character
- * @C: The charcter tobe checked
+ * @c: The charcter tobe checked
  * Return: 1 for lowercase character or a 0 for anything else
 */
 
@@ -10,7 +10,6 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-	
 	return (1);
 	}
 	return (0);
