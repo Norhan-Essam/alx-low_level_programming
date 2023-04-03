ff89a22f6ea844951b81ac4da53fdb73204b9f57
Norhn <nourhanessam102@gmail.com>
2023-04-03 14:17:37 -0700

    k

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_N
+#define MAIN_N
 
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
