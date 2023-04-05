8678d30ec8571b86e216baaa9395325c94a704df
Norhn <nourhanessam102@gmail.com>
2023-04-05 11:45:08 -0700

    j

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @a: pointer to the string
+ * *Return: void
+*/
+
+void _print_rev_recursion(char *a)
+{
+	if (*a > '\0')
+	{
+		_print_rev_recursion(a + 1);
+		_putchar(*a);
+	}
+}
