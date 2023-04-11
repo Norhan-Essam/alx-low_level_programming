2d53e47b22d5020dd31e9e863a36f73926b7de9b
Norhn <nourhanessam102@gmail.com>
2023-04-11 14:04:51 -0700

    y

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
+*/
+
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+	;
+	return (size);
+}
+
+/**
+ * *argstr - description
+ * @ac: int
+ * @av: arguments
+ * Return: string
+*/
+
+
+char *argstr(int ac, char **av)
+{
+	int a = 0, nc = 0, e = 0, cmpt = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (; a < ac; a++, nc++)
+		nc += _strlen(av[a]);
+
+	s = malloc(sizeof(char) * nc + 1);
+	if (s == 0)
+		return (NULL);
+
+	for (a = 0; a < ac; a++)
+	{
+		for (e = 0; av[a][e] != '\0'; e++, cmpt++)
+			s[cmpt] = av[a][e];
+
+		s[cmpt] = '\n';
+		cmpt++;
+	}
+	s[cmpt] = '\0';
+	return (s);
+}
