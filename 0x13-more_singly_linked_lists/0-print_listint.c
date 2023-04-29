102ba2e7d0413b411486f0b727637a7742b6299d
Norhn <nourhanessam102@gmail.com>
2023-04-28 22:33:34 -0700

    h

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * -strlen - returns the length of a string
+ *  @d: the string whose length to check
+ *
+ *  Return: integer length of string
+*/
+int _strlen(char *d)
+{
+	int e = 0;
+
+	if (!d)
+		return (0);
+	while (*d++)
+		e++;
+	return (e);
+}
+
+/**
+ * print_list - prints a linked lists
+ * @g: pointer to first node
+ *
+ * Return: size of list
+*/
+size_t print_list(const list_t *g)
+{
+	size_t e = 0;
+
+	while (g)
+	{
+		printf("[%d] %s\n", _strlen(g->str), g->str ? g->str : "(nil)");
+		g = g->next;
+		e++;
+	}
+	return (e);
+}
