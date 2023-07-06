ca1b2442eb018b4bca933b8e01df6ee14e7b153f
Your Name <you@example.com>
2023-07-06 11:10:25 -0700

    u

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,17 +2,17 @@
 
 /**
  * print_binary - prints a number as binary string
- * @m: the number to print
+ * @n: the number to print
  *
  * Return: void
 */
-void print_binary(unsigned long int m)
+void print_binary(unsigned long int n)
 {
-	int bit = sizeof(m) * 8, printed = 0;
+	int bit = sizeof(n) * 8, printed = 0;
 
 	while (bit)
 	{
-		if (m && 1L << --bit)
+		if (n && 1L << --bit)
 		{
 			_putchar('1');
 			printed++;
