91ed41262dec1ac62f93b6801b289a5d971ec883
Your Name <you@example.com>
2023-07-10 07:21:34 -0700

    d

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,16 +26,16 @@ int _strlen(char *z)
 */
 int create_file(const char *filename, char *text_content)
 {
-	int ed;
+	int fd;
 	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-	ed = open(filename, O_WRONLY | O_CREAT | O_TRUNC, O_TRUNC | S_IWUSR);
-	if (ed == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, O_TRUNC | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 	if (len)
-		bytes = write(ed, text_content, len);
-	close(ed);
+		bytes = write(fd, text_content, len);
+	close(fd);
 	return (bytes == len ? 1 : -1);
 }
