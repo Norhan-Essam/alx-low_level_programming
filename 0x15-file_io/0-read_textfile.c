6726e12387e01128c6d75f12af45cfa6e2b73a91
Your Name <you@example.com>
2023-07-10 07:26:22 -0700

    d

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,19 +7,19 @@
  *
  * Return: number bytes read/printed
 */
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int eb;
+	int fd;
 	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
-	eb = open(filename, O_RDONLY);
-	if (eb == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	bytes = read(eb, &buf[0], letters);
+	bytes = read(fd, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	close(eb);
+	close(fd);
 	return (bytes);
 }
