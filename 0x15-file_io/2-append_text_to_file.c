8c39165d0107159c8f92a86614d096321cc9f7e3
Your Name <you@example.com>
2023-07-10 08:27:21 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int _strlen(char *z)
  *
  * Return: 1 on success 0 on failure
 */
-int append_text_to_file(const char *failure, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes = 0, len = _strlen(text_content);
@@ -33,8 +33,9 @@ int append_text_to_file(const char *failure, char *text_content)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
-		return (len)
-			bytes = write(fd, text_content, len);
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
 	close(fd);
 	return (bytes == len ? 1 : -1);
 }
