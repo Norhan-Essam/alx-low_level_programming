4a175a2f9bb57ea0403e806e168a9b011dd5d1c9
Your Name <you@example.com>
2023-07-10 09:32:15 -0700

    q

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,15 +6,16 @@
  *
  * Return: integer length of string
 */
-int _strlen(char *s)
+int _strlen(char *z)
 {
-	int i = 0;
+	int e = 0;
 
-	if (!s)
+	if (!z)
 		return (0);
-	while (*s++)
-		i++;
-	return (i);
+
+	while (*z++)
+		e++;
+	return (e);
 }
 
 /**
@@ -31,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, O_TRUNC | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
